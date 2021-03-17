4588f1b218ea4e41e483c7d9b4e7679d46fc34f0
Isaac H. J. Houngue <hjisaac>
2021-03-17 20:47:06 +0100

    implementation of graphqlServer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-// index.js
-// This is the main entry point of our application
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+const typeDefs = gql`
+    type Note
+    type Query {
+        hello: String
+    }
+
+    type Query {
+        Note: {
+
+        }
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello world"
+    }
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+apolloServer.applyMiddleware({ app, path: "/api" })
+
+
+app.listen(
+    { port },
+    () => {
+        console.log(`GraphQL Server listening at http://localhost:${port}${apolloServer.graphqlPath}`);
+    }
+);
