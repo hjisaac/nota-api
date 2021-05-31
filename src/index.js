2d066cfb1f382974067c8d57e912f83c2ae9150a
Isaac H. J. Houngue <hjisaac>
2021-05-31 11:38:51 +0100

    cors problem solving with heroku

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ const apolloServer = new ApolloServer(
         }
     }
 );
-apolloServer.applyMiddleware({ app, path:"/api" });
+apolloServer.applyMiddleware({ app, path:"/api", cors: { corsOptions } });
 
 app.listen(
     { port },
